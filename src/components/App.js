d954a540c02acc4db46308447722dc523e0846a1
Derrick Strong <derrickmstrong@gmail.com>
2021-09-21 09:34:51 -0500

    Setup streams routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,21 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
-const PageOne = () => {
-    return <div>Page 1
-        <Link to='/pagetwo'>Go to Page 2</Link>
-    </div>
-}
-const PageTwo = () => {
-    return (
-      <div>
-        Page 2<button>Click Me</button>
-        <Link to='/'>Go to Page 1</Link>
-      </div>
-    );
-}
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import StreamCreate from './streams/StreamCreate'
+import StreamEdit from './streams/StreamEdit'
+import StreamDelete from './streams/StreamDelete'
+import StreamList from './streams/StreamList'
+import StreamShow from './streams/StreamShow'
+
 const App = () => {
     return (
         <div className='ui container'>
             <h1>Streams</h1>
             <Router>
-                <Route path='/' exact component={PageOne} />
-                <Route path='/pagetwo' exact component={PageTwo} />
+                <Route path='/' exact component={StreamList} />
+                <Route path='/streams/new' exact component={StreamList} />
+                <Route path='/streams/edit' exact component={StreamEdit} />
+                <Route path='/streams/delete' exact component={StreamDelete} />
+                <Route path='/streams/show' exact component={StreamShow} />
             </Router>
         </div>
     )
